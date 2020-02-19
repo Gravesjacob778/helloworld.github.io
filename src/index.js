f11b737aa1a2bb1d4d5df665de91f96b4869d516
Graves <bigbang77855@gmail.com>
2020-02-19 18:22:38 +0800

    add index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const multiButton=()=>{
+    var output = [];
+    for (let i = 0; i < 5; i++){
+        output.push(<button>按鍵i</button>)
+    }
+    return output;
+}
+
+ReactDOM.render(
+    <div>{multiButton()}</div>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
