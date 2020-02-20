10304b04603f074915aef57de431bc0e5be2aae4
Graves <bigbang77855@gmail.com>
2020-02-20 18:01:05 +0800

    JAX

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'; //引入App
 import * as serviceWorker from './serviceWorker';
 
-const multiButton=()=>{
-    var output = [];
-    for (let i = 0; i < 5; i++){
-        output.push(<button>按鍵i</button>)
-    }
-    return output;
-}
 
 ReactDOM.render(
-    <div>{multiButton()}</div>,
+    <div>
+        <App/>
+    </div>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
